a0e209a6230b7a324dfd28de9e1359148032bd63
Abubakar <163635950+Abum25@users.noreply.github.com>
2024-04-11 23:30:57 +0200

    Öpnna window

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
-int main() {
-    printf("Hello! \n");
-    return 0;
+
+
+
+int main(int argv, char** args) {
+
+    
+    if(SDL_Init(SDL_INIT_VIDEO)!= 0) {
+        printf("Error: %s\n", SDL_GetError());
+        return 1;
+    }
 }
+
