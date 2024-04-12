d5386b358922b5ac9a537f56f18b5cec98bd2734
Jonathan Debessai <jonathan.debessaii@gmail.com>
2024-04-12 13:46:45 +0200

    skapade en grund för main.c och ball.c

--- Makefile
+++ Makefile
@@ -6,8 +6,8 @@ INCLUDE = C:\msys64\mingw64\include\SDL2
 CFLAGS = -g -I$(INCLUDE) -c 
 LDFLAGS = -lmingw32 -lSDL2main -lSDL2_image -lSDL2 -mwindows -lm
 
-simpleSDLexample2: main.o
-	$(CC) main.o -o simpleSDLexample2 $(LDFLAGS)
+footballGame: main.o
+	$(CC) main.o -o footballGame $(LDFLAGS)
 
 main.o: $(SRCDIR)\main.c
 	$(CC) $(CFLAGS) $(SRCDIR)\main.c
---
+++ source/ball.c
@@ -0,0 +1,22 @@
+#include <SDL2/SDL.h>
+#include <SDL_image.h>
+#include "ball.h"
+#include <string.h>
+
+struct ball{
+    float x, y, vx, vy; //koordinater samt hastighet i y- och x-led
+    int hasCollided;
+    char currentBallImage[4][20];
+};
+
+Ball *createBall(SDL_Renderer *pRenderer) {
+    Ball *pBall = malloc(sizeof(struct ball));
+    pBall->x = pBall->y = pBall->vy = pBall->vx = 0; //initierar alla värden till 0
+    strcpy(resources/ball0.png, "resources/ball0.png");
+    SDL_Surface *pSurface = IMG_Load(pBall->currentBallImage[0]);
+    if(!pSurface) {
+        printf("Error: %s\n", SDL_GetError());
+        return NULL; //använd NULL istället för 0 i funktioner som returnerar pekare
+    }
+    return pBall;
+}
