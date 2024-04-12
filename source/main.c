34f5a083c7fd919720d89379b41d4b3f230bf63e
Abubakar <163635950+Abum25@users.noreply.github.com>
2024-04-12 16:11:13 +0200

    ändring

--- source/main.c
+++ source/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include "../include/ball.h"
 #define WINDOW_WIDTH 1600
 #define WINDOW_HEIGHT 900
@@ -15,12 +16,13 @@ typedef struct game {
 
 int initiate(Game *pGame);
 void close(Game *pGame);
+void run(Game *pGame);
 
 int main(int argv, char** args) {
     Game g = {0};
     if(!initiate(&g)) return 1;
-    //run(&g);
-    exit(&g);
+    run(&g);
+    close(&g);
     
     return 0;
 }
@@ -30,7 +32,7 @@ int initiate(Game *pGame) {
         printf("Error: %s\n",SDL_GetError());
         return 0;
     }
-    pGame->pWindow = SDL_CreateWindow("Rocket Game",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WINDOW_WIDTH,WINDOW_HEIGHT,0);
+    pGame->pWindow = SDL_CreateWindow("Football Game",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WINDOW_WIDTH,WINDOW_HEIGHT,0);
     if(!pGame->pWindow){
         printf("Error: %s\n",SDL_GetError());
         close(pGame);
@@ -42,37 +44,52 @@ int initiate(Game *pGame) {
         close(pGame);
         return 0;    
     }
-
-
-    SDL_Surface *backgroundSurface = IMG_Load("resources/field.png");
+    SDL_Surface *backgroundSurface = IMG_Load("resources/topdownfield.png");
     if(!backgroundSurface){
         printf("Error: %s\n",SDL_GetError());
         close(pGame);
         return 0;    
     }
 
-    SDL_Texture *backgroundTexture = SDL_CreateTextureFromSurface(pGame->pRenderer, backgroundSurface);
+    pGame->backgroundTexture = SDL_CreateTextureFromSurface(pGame->pRenderer, backgroundSurface);
     SDL_FreeSurface(backgroundSurface);
-    if(!backgroundTexture){
+    if(!pGame->backgroundTexture){
         printf("Error: %s\n",SDL_GetError());
         close(pGame);
         return 0;    
     }
 
-    pGame->pBall = createBall(pGame->pRenderer);
+    //pGame->pBall = createBall(pGame->pRenderer);
     //pGame->pPlayer = createAsteroidImage(pGame->pRenderer);
 
-    if(!pGame->pBall /*|| !pGame->pPlayer*/){
+    /*if(!pGame->pBall /*|| !pGame->pPlayer){
         printf("Error: %s\n",SDL_GetError());
         close(pGame);
         return 0;
-    }
+    }*/
 
     /*for(int i=0;i<NROFPLAYERS;i++){
         pGame->pAsteroids[i] = createAsteroid(pGame->pAsteroidImage,WINDOW_WIDTH,WINDOW_HEIGHT);
     }*/
 
     return 1;
+}
+
+void run(Game *pGame) {
+    int close_requested = 0;
+    SDL_Event event;
+
+    while (!close_requested) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) close_requested = 0;
+            //else handleInput(pGame,&event);
+        }
+        //updateBall(pGame->pBall);
+        SDL_RenderClear(pGame->pRenderer);
+        SDL_RenderCopy(pGame->pRenderer, pGame->backgroundTexture, NULL, NULL);
+        SDL_RenderPresent(pGame->pRenderer);
+        SDL_Delay(1000/60-15);
+    }
 }
 
 void close(Game *pGame) {
