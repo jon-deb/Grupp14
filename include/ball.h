e657796b5a6d29df5127e3fcceadd93d9d03cff5
Abubakar <163635950+Abum25@users.noreply.github.com>
2024-04-14 12:07:06 +0200

    tog bort structen i ball.h

--- include/ball.h
+++ include/ball.h
@@ -4,16 +4,13 @@
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 
-typedef struct ball {
-    SDL_Texture *texture;
-    SDL_Rect rect;
-    float velocityX;
-    float velocityY;
-    bool collided;
-} Ball;
+typedef struct ball Ball;
 
 Ball *createBall(SDL_Renderer *renderer);
 void updateBallPosition(Ball *ball);
 void destroyBall(Ball *ball);
+
+SDL_Rect getBallRect(Ball *ball);
+SDL_Texture *getBallTexture(Ball *ball);
 
 #endif /* BALL_H */
