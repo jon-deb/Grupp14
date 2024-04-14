eea0228f1ca58554e790dc309a59e378f5d8cb58
Abubakar <163635950+Abum25@users.noreply.github.com>
2024-04-14 14:54:02 +0200

    fixade collision

--- include/ball.h
+++ include/ball.h
@@ -1,16 +1,17 @@
 #ifndef BALL_H
 #define BALL_H
 
-#include <stdbool.h>
 #include <SDL2/SDL.h>
+#include <stdbool.h>
 
 typedef struct ball Ball;
 
 Ball *createBall(SDL_Renderer *renderer);
 void updateBallPosition(Ball *ball);
 void destroyBall(Ball *ball);
-
-SDL_Rect getBallRect(Ball *ball);
 SDL_Texture *getBallTexture(Ball *ball);
+SDL_Rect getBallRect(Ball *ball);
+void setBallVelocity(Ball *ball, float velocityX, float velocityY);
+void setBallVelocity(Ball *ball, float velocityX, float velocityY);
 
 #endif /* BALL_H */
